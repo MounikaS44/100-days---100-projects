5cd419f9859efe508c617bfa65d2f697f25c5760
MounikaS44 <surampudimounika44@gmail.com>
2026-08-18 15:02:30 +0530

    Day 1 of 100 - Personal Portfolio

--- 1. Portfolio/js/main.js
+++ 1. Portfolio/js/main.js
@@ -129,7 +129,7 @@
 document.getElementById('contactForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
-    var GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLScK0w9HFKRt-UzzFmvPP_t6QigfIvuaRX-LJjmrcKPjRu3eoQ/viewform?usp=publish-editor';
+    var GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLScK0w9HFKRt-UzzFmvPP_t6QigfIvuaRX-LJjmrcKPjRu3eoQ/viewform?usp=dialog';
 
     var formData = new FormData();
     formData.append('entry.2005620554', document.getElementById('name').value);   
